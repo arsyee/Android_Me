1b2413b732409e984ac26ce659327167b8e8f875
arsyee <2476948+arsyee@users.noreply.github.com>
2018-03-24 18:00:08 +0100

    first solution (wrong heights)

---
+++ app/src/main/java/com/example/android/android_me/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.android_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnListFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(Integer item) {
+
+    }
+}
---
+++ app/src/main/java/com/example/android/android_me/MasterListAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.android_me;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.MasterListFragment.OnListFragmentInteractionListener;
+
+import java.util.List;
+
+public class MasterListAdapter extends RecyclerView.Adapter<MasterListAdapter.ViewHolder> {
+
+    private final List<Integer> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MasterListAdapter(List<Integer> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_masterlist, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setImageResource(mValues.get(position));
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mIdView;
+        public int mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (ImageView) view.findViewById(R.id.iv_image);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mItem + "'";
+        }
+    }
+}
